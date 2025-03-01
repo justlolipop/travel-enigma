640e7b256745604ebb04220c8a319b08efd18826
Steven-Low <2002lowsy@gmail.com>
2025-03-01 22:28:41 +0800

    all

---
+++ frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,13 @@
+
+const AboutUs = () => {
+    return (
+        <div>
+            <h1>About Us</h1>
+            <p>
+                We are a group of developers who are passionate about coding and building web applications.
+            </p>
+        </div>
+    );
+}
+
+export default AboutUs;
